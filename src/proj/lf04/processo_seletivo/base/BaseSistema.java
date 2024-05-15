ef735e7a4e76bea43e4a8b9d250f28212116b873
@luisfl04_ <@luisfelipecontato08@gmail.com>
2024-05-15 15:45:10 -0300

    feat: Criação do método 'verificar_idade()' em VerificacaoDados. Para verificar valores de idades inválidos.

--- src/proj/lf04/processo_seletivo/base/BaseSistema.java
+++ src/proj/lf04/processo_seletivo/base/BaseSistema.java
@@ -7,54 +7,47 @@ import java.util.InputMismatchException;
 
 // Classe onde os métodos que serão nescessários para o funcionamento do sistema serão implementados.
 public class BaseSistema {
-    
+
     // Atributos globais que irão receber valores e serem mostrados na interface do sistema:
     public static String nome_candidato;
     public static int idade_candidato;
     public static double salario_pretendido_candidato;
 
     // Método que vai pedir as informações gerais do candidato, como: nome, salário pretendido e etc. É o métodp introdutório do sistema.
-    static void pegar_informacoes_candidato(){
-
-        // Criando instância da classe 'VerificacaoDados' para chamar os métodos de verificação:
-        VerificacaoDados verificacao = new VerificacaoDados();
+    public static void pegar_informacoes_candidato(){
         
-        // Abrindo instância da classe 'Scanner':
+        // Intâncias nescessárias para o funcionamento:
         Scanner scanf = new Scanner(System.in);
+        VerificacaoDados verificacao = new VerificacaoDados();
         
         // Pedindo as informações pelo teclado:
         // Nome: -> (Não é nescessário fazer tratamento de erros em valores 'String', pois irá ser aceito qualquer tipo de valor).
         System.out.println("Olá candidato, primeiramente nos informe o seu nome.\n* Somente o primeiro nome *\n");
         nome_candidato = scanf.nextLine();
 
-        // idade:
+        // idade: -> Em todos os casos eu faço a verificação do valor. Tanto para valores numéricos inválidos, quanto para valores que não são do tipo inteiro.
         try{
-            System.out.println("Certo " + nome_candidato + ", nos informe a sua idade abaixo:");
+            System.out.println("\nCerto " + nome_candidato + ", nos informe a sua idade abaixo:");
             idade_candidato = scanf.nextInt();
             verificacao.verificar_idade(idade_candidato);
         }
         catch(InputMismatchException exception){
-            System.out.println("\nERRO -> * O valor inserido é inválido *\nInsira novamente abaixo:");
-            idade_candidato = scanf.nextInt();
+            System.out.println("\nERRO -> * Você digitou um valor com tipo inesperado. * ");
+            verificacao.verificar_idade(idade_candidato);
         }
-        // finally{  
-        //     // Independentemente do fluxo, irei chamar este método. Para fins de tratamento em relação ao valor que o usuário inserir.
-        //     verificacao.verificar_idade(idade_candidato);
-        // }   
-
-
 
         // Fechando instância de 'Scanner':
         scanf.close();
         
     }
-
-
-
+    
+    
+    
     // Método que compara e analisa o salário pretendido pelo canditado com relação ao salário base:
-    static void analisar_salario_candidato(double salario_pretendido_candidato){
+    public static void analisar_salario_candidato(double salario_pretendido_candidato){
         
     } 
+    
 
 }
 
@@ -66,12 +59,26 @@ class VerificacaoDados {
 
     // Método que verifica se o valor que o candidato informou para a sua idade é inválido:
     void verificar_idade(int idade_informada_pelo_usuario){
-
+        
+        // Criação de variável 'entrada_validada' para validar a entrada do usuário somente quando o valor satisfazer todas as condições:
+        boolean entrada_validada = false;
+        
         // Fazendo verificação e pedindo um valor válido:
-        while(idade_informada_pelo_usuario < 1 || idade_informada_pelo_usuario > 100){
-            // Informando erro e pedindo o valor novamente:
-            System.out.println("\nO valor que você inseriu é inválido!\nDigite um valor válido abaixo:");
-            idade_informada_pelo_usuario = scanf.nextInt();   
+        while(idade_informada_pelo_usuario < 1 || idade_informada_pelo_usuario > 100 || entrada_validada == false){
+            
+            // Informando erro e tentando pedir o valor novamente:
+            try{
+                System.out.println("\nO valor inserido é invalido!\nInsira um valor válido abaixo:");
+                idade_informada_pelo_usuario = scanf.nextInt();
+                entrada_validada = true;
+            }
+            catch(InputMismatchException exception){
+                // Informando erro e limpando o 'scanner':
+                scanf.next();
+            }
+
         }
+    
     }   
+
 }
--- src/proj/lf04/processo_seletivo/face/ProcessoSeletivo.java
+++ src/proj/lf04/processo_seletivo/face/ProcessoSeletivo.java
@@ -1,11 +1,14 @@
-package proj.lf04.processo_seletivo.interface
+package proj.lf04.processo_seletivo.face;
+
+// Importações nescessárias:
+import proj.lf04.processo_seletivo.base.BaseSistema;
 
 
+// Classe que irá ser responsável por chamar os métodos e 'conversar' com o usuário:
 public class ProcessoSeletivo{
-
     public static void main(String[] args) {
 
+        BaseSistema.pegar_informacoes_candidato();
+
     }
-
-
 }
